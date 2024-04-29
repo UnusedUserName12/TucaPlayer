57d2ee98eae6f61154d4ee67774551fad4b48695
ilia <turcan92ilia@gmail.com>
2024-04-29 17:40:52 +0300

    Creating local database

--- app/src/main/java/com/example/myapplication/db/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/db/DatabaseHelper.java
@@ -1,2 +1,67 @@
-package com.example.myapplication.db;public class DatabaseHelper {
+package com.example.myapplication.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    static final String DATABASE_NAME = "TUCAPLAYER.db";
+    static int DATABASE_VERSION = 1;
+
+    public static final String PLAYLIST_TABLE = "PLAYLISTS";
+    public static final String PLAYLIST_ID = "playlist_id";
+    public static final String PLAYLIST_NAME = "playlist_name";
+    public static final String PLAYLIST_PICTURE = "playlist_picture";
+
+    private static final String CREATE_PLAYLIST_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS "+PLAYLIST_TABLE
+            +"("
+            + PLAYLIST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + PLAYLIST_NAME + " TEXT NOT NULL, "
+            + PLAYLIST_PICTURE + " TEXT"
+            + ");";
+
+
+    public static final String SONG_TABLE = "SONGS";
+    public static final String SONG_ID = "song_id";
+    public static final String SONG_FILENAME = "song_filename";
+
+    private static final String CREATE_SONG_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " + SONG_TABLE
+            + "("
+            + SONG_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + SONG_FILENAME + " TEXT NOT NULL"
+            + ");";
+
+
+    public static final String PLAYLIST_SONGS_TABLE = "PLAYLIST_SONGS";
+    public static final String PLAY_SONGS_TABLE_PLAYLIST_ID = "playlist_id";
+    public static final String PLAY_SONGS_TABLE_SONG_ID = "song_id";
+
+    private static final String CREATE_PLAYLIST_SONGS_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " + PLAYLIST_SONGS_TABLE
+            +"("
+            + PLAY_SONGS_TABLE_PLAYLIST_ID + " INTEGER, "
+            + PLAY_SONGS_TABLE_SONG_ID + " INTEGER,"
+            + "FOREIGN KEY ("+PLAY_SONGS_TABLE_PLAYLIST_ID+") REFERENCES " + PLAYLIST_TABLE + "(" + PLAYLIST_ID + ") ON DELETE CASCADE,"
+            + "FOREIGN KEY ("+PLAY_SONGS_TABLE_SONG_ID+") REFERENCES " + SONG_TABLE + "(" + SONG_ID + ") ON DELETE CASCADE"
+            + ");";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PLAYLIST_TABLE_QUERY);
+        db.execSQL(CREATE_SONG_TABLE_QUERY);
+        db.execSQL(CREATE_PLAYLIST_SONGS_TABLE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ PLAYLIST_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS "+ SONG_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS "+ PLAYLIST_SONGS_TABLE);
+    }
 }
--- app/src/main/java/com/example/myapplication/db/DatabaseManager.java
+++ app/src/main/java/com/example/myapplication/db/DatabaseManager.java
@@ -1,2 +1,92 @@
-package com.example.myapplication.db;public class DatabaseManager {
+package com.example.myapplication.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class DatabaseManager  {
+    private DatabaseHelper dbHelper;
+    private Context context;
+    private SQLiteDatabase database;
+    public DatabaseManager(Context ctx){
+        this.context = ctx;
+    }
+
+    public DatabaseManager open() throws SQLDataException {
+        dbHelper = new DatabaseHelper(context);
+        database = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close(){
+        dbHelper.close();
+    }
+
+    public void insertPlaylist(String name, String picture_name){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.PLAYLIST_NAME, name);
+        contentValues.put(DatabaseHelper.PLAYLIST_PICTURE, picture_name);
+        database.insert(DatabaseHelper.PLAYLIST_TABLE,null,contentValues);
+    }
+
+    public void insertSong(String filename){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseHelper.SONG_FILENAME,filename);
+        database.insert(DatabaseHelper.SONG_TABLE,null,contentValues);
+    }
+
+
+    //WARNING - this will break?
+    public void insertPlaylistSong(int playlist_id, int song_id){
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(DatabaseHelper.PLAY_SONGS_TABLE_PLAYLIST_ID,playlist_id);
+        contentValues.put(DatabaseHelper.PLAY_SONGS_TABLE_SONG_ID,song_id);
+        database.insert(DatabaseHelper.PLAYLIST_SONGS_TABLE,null,contentValues);
+    }
+
+    public Cursor fetchPlaylists(){
+        String [] columns = new String[] {DatabaseHelper.PLAYLIST_ID,DatabaseHelper.PLAYLIST_NAME,DatabaseHelper.PLAYLIST_PICTURE};
+        Cursor cursor = database.query(DatabaseHelper.PLAYLIST_TABLE, columns,null,null,null,null,null,null);
+        if (cursor !=null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public Cursor fetchSongs(){
+        String [] columns = new String[] {DatabaseHelper.SONG_ID,DatabaseHelper.SONG_FILENAME};
+        Cursor cursor = database.query(DatabaseHelper.SONG_TABLE, columns,null,null,null,null,null,null);
+        if (cursor !=null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public Cursor fetchPlaylistSongs(){
+        String [] columns = new String[] {DatabaseHelper.PLAY_SONGS_TABLE_PLAYLIST_ID,DatabaseHelper.PLAY_SONGS_TABLE_SONG_ID};
+        Cursor cursor = database.query(DatabaseHelper.PLAYLIST_SONGS_TABLE, columns,null,null,null,null,null,null);
+        if (cursor !=null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+
+    public void deletePlaylist(int id){
+        database.delete(DatabaseHelper.PLAYLIST_TABLE, DatabaseHelper.PLAYLIST_ID + "="+id,null);
+    }
+
+    public void deleteSong(int id){
+        database.delete(DatabaseHelper.SONG_TABLE, DatabaseHelper.SONG_ID + "="+id,null);
+    }
+
+    public void deletePlaylistSong(int playlist_id, int song_id){
+        database.delete(DatabaseHelper.PLAYLIST_SONGS_TABLE,DatabaseHelper.PLAY_SONGS_TABLE_PLAYLIST_ID + " = "+playlist_id+" AND "+DatabaseHelper.PLAY_SONGS_TABLE_SONG_ID+"="+song_id,null);
+    }
+
+
 }
