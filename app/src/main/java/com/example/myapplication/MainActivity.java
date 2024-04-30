a2748800cbe189c0f729d9d8a972de9ff0fd3bab
ilia <turcan92ilia@gmail.com>
2024-04-30 12:05:50 +0300

    Implemented automatic database song insert

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -13,11 +18,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.myapplication.db.DatabaseHelper;
+import com.example.myapplication.db.DatabaseManager;
 import com.google.android.material.tabs.TabLayout;
 
 import java.io.File;
@@ -70,8 +78,12 @@ public class MainActivity extends AppCompatActivity {
 
 
         if (!checkPermission()) {
-            requestPermission();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                requestPermission();
+            }
         }
+
+        updateSongTable();
     }
 
     private boolean checkPermission() {
@@ -79,10 +91,76 @@ public class MainActivity extends AppCompatActivity {
                 == PackageManager.PERMISSION_GRANTED;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     private void requestPermission() {
         ActivityCompat.requestPermissions(this,
                 new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
                 PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.READ_MEDIA_AUDIO},
+                PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(this,
+                new String[] {Manifest.permission.READ_MEDIA_IMAGES},
+                PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void updateSongTable() {
+
+        //Checking the folder
+        List<File> FolderFileList = new ArrayList<>();
+        File downloadsFolder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        if (downloadsFolder.exists() && downloadsFolder.isDirectory()) {
+            File[] files = downloadsFolder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile() && file.getName().toLowerCase().endsWith(".mp3")) {
+                        FolderFileList.add(file);
+                    }
+                }
+
+            }
+        } else {
+            Toast.makeText(this, "Downloads folder not found", Toast.LENGTH_SHORT).show();
+        }
+
+        //Checking the database
+        DatabaseManager databaseManager = new DatabaseManager(this);
+        try{
+            databaseManager.open();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Cursor cursor = databaseManager.fetchSongs();
+        List<String> dbFileList = new ArrayList<>();
+        if(cursor.moveToFirst()){
+            do {
+                @SuppressLint("Range") String dbFILENAME = cursor.getString(cursor.getColumnIndex(DatabaseHelper.SONG_FILENAME));
+                dbFileList.add(dbFILENAME);
+                Toast.makeText(this,dbFILENAME,Toast.LENGTH_SHORT).show();
+                Log.i("DATABASE_TAG", "I have read SONG : "+dbFILENAME);
+            }while (cursor.moveToNext());
+        }
+
+        //Compare database and folder
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        for (File file : FolderFileList) {
+            Boolean found = false;
+            for (String filename : dbFileList) {
+                if(file.getName().equals(filename)) found=true;
+            }
+            if(!found){
+                mmr.setDataSource(file.getPath());
+                String song_name = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+                String song_album = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+                String song_artist = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+                String song_genre = mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE);
+
+                databaseManager.insertSong(file.getName(),song_name,song_album,song_artist,song_genre);
+            }
+        }
+
+        databaseManager.close();
     }
 
 }
