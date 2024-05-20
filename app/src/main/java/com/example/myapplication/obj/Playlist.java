75e150ae427c9398b4bbb51538cd124ce4d8a47c
ilia <turcan92ilia@gmail.com>
2024-05-20 12:12:38 +0300

    Finally added some usage to this class (TODO: check if actually useful)

--- app/src/main/java/com/example/myapplication/obj/Playlist.java
+++ app/src/main/java/com/example/myapplication/obj/Playlist.java
@@ -1,14 +1,21 @@
 package com.example.myapplication.obj;
 
+import java.util.List;
+
+//TODO: Check if actually useful
 public class Playlist {
+    private int id;
     private String name;
+    private String image_path;
 
-    private int image;
-
-    public Playlist(String name, int image) {
+    public Playlist(int id, String name, String image) {
+        this.id = id;
         this.name = name;
-        this.image = image;
+        this.image_path = image;
     }
+
+    public int getId(){return id;}
+    public void setId(int id){this.id = id;}
 
     public String getName() {
         return name;
@@ -18,11 +25,12 @@ public class Playlist {
         this.name = name;
     }
 
-    public int getImage() {
-        return image;
+    public String getImagePath() {
+        return image_path;
     }
 
-    public void setImage(int image) {
-        this.image = image;
+    public void setImagePath(String image_path) {
+        this.image_path = image_path;
     }
+
 }
