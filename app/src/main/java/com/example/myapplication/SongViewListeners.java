7df9a9c38829ddd6dec2cf8a4dc8a091657640f6
ilia <turcan92ilia@gmail.com>
2024-06-06 13:10:23 +0300

    SongViewListeners: fixed typo

--- app/src/main/java/com/example/myapplication/SongViewListeners.java
+++ app/src/main/java/com/example/myapplication/SongViewListeners.java
@@ -143,7 +143,7 @@ public class SongViewListeners implements OnSongChangeListener {
 
         btnShrink.setOnClickListener(v -> {
             if (isExpanded) {
-                expandSong();
+                shrinkSong();
             }
         });
 
