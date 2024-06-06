2347511f73d40223c8f9b288f4f316ff9fbc9be8
ilia <turcan92ilia@gmail.com>
2024-06-06 13:09:57 +0300

    PlaySongActivity: deprecated

--- app/src/main/java/com/example/myapplication/PlaySongActivity.java
+++ app/src/main/java/com/example/myapplication/PlaySongActivity.java
@@ -1,6 +1,20 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+* <p>Deprecated class</p>
+ * <p>Watch SongViewListeners<p>
+ */
 
 public class PlaySongActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_song_view);
+    }
 }
